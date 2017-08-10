49498d6f9a974962dcf04663707bde66d35eaaab
System <none>
2017-08-10 06:15:59 +0000

    null

--- index.js
+++
@@ -1,8 +0,0 @@
-require('babel-register');
-
-const app = require('./src/app').app,
-      PORT = process.env.PORT || 8000;
-
-app.listen(PORT, function() {
-  console.log('Server is listening on port ' + PORT);
-});
